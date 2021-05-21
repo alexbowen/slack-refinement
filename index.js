486d0b1be567d5f36aba6f774a00f02b9a895fb9
alexbowen <alexbowen123@googlemail.com>
2021-05-21 23:07:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+// This example shows how to listen to a button click
+// It uses slash commands and actions
+// Require the Bolt package (github.com/slackapi/bolt)
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: "xoxb-170869138993-2073304885155-f0yjD8xr9cQoTzP4nzTpKICU",
+  signingSecret: "e3d3e25bfdd10975761b8ed0c37fc03e"
+});
+
+// Listen for a slash command invocation
+app.command('/estimate', async ({ ack, payload, context }) => {
+  // Acknowledge the command request
+  ack();
+
+  console.log('estimate');
+
+  try {
+    const result = await app.client.chat.postMessage({
+      token: context.botToken,
+      // Channel to send message to
+      channel: payload.channel_id,
+      // Include a button in the message (or whatever blocks you want!)
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: 'Go ahead. Click it.'
+          },
+          accessory: {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'Click me!'
+            },
+            action_id: 'button_abc'
+          }
+        }
+      ],
+      // Text in the notification
+      text: 'Message from Test App'
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+// Listen for a button invocation with action_id `button_abc`
+// You must set up a Request URL under Interactive Components on your app configuration page
+app.action('estimate', async ({ ack, body, context }) => {
+  // Acknowledge the button request
+  ack();
+
+  console.log('estimate action');
+
+  try {
+    // Update the message
+    const result = await app.client.chat.update({
+      token: context.botToken,
+      // ts of message to update
+      ts: body.message.ts,
+      // Channel of message
+      channel: body.channel.id,
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: '*The button was clicked!*'
+          }
+        }
+      ],
+      text: 'Message from Test App'
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log(app)
+
+  console.log('⚡️ Bolt app is running!');
+})();
