40512f0b4e0e0137386297224c5afb28dfaff85c
alexbowen <alexbowen123@googlemail.com>
2021-05-22 01:18:38 +0100

    new bot token

--- index.js
+++ index.js
@@ -98,8 +98,24 @@ app.shortcut('refinement', async ({ shortcut, ack, client }) => {
 
 app.action('submit', async ({ ack, say }) => {
   // Acknowledge action request
-  await ack();
-  await say('Estimation submited 👍');
+  
+  try {
+
+    await ack();
+    
+    await app.client.chat.postMessage({
+      token: context.botToken,
+      // Channel to send message to
+      channel: payload.channel_id,
+      // Include a button in the message (or whatever blocks you want!)
+
+      // Text in the notification
+      text: 'Estimation submitted'
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
 });
 
 (async () => {
@@ -107,3 +123,77 @@ app.action('submit', async ({ ack, say }) => {
 
   console.log('Ticket refinement app is running!');
 })();
+
+
+// app.command('/estimate', async ({ ack, payload, context }) => {
+//   // Acknowledge the command request
+//   ack();
+
+//   console.log('estimate command');
+
+//   try {
+//     const result = await app.client.chat.postMessage({
+//       token: context.botToken,
+//       // Channel to send message to
+//       channel: payload.channel_id,
+//       // Include a button in the message (or whatever blocks you want!)
+//       blocks: [
+//         {
+//           type: 'section',
+//           text: {
+//             type: 'mrkdwn',
+//             text: 'Go ahead. Click it.'
+//           },
+//           accessory: {
+//             type: 'button',
+//             text: {
+//               type: 'plain_text',
+//               text: 'Click me!'
+//             },
+//             action_id: 'button_abc'
+//           }
+//         }
+//       ],
+//       // Text in the notification
+//       text: 'Message from Test App'
+//     });
+//     console.log(result);
+//   }
+//   catch (error) {
+//     console.error(error);
+//   }
+// });
+
+// // Listen for a button invocation with action_id `button_abc`
+// // You must set up a Request URL under Interactive Components on your app configuration page
+// app.action('estimate', async ({ ack, body, context }) => {
+//   // Acknowledge the button request
+//   ack();
+
+//   console.log('estimate action');
+
+//   try {
+//     // Update the message
+//     const result = await app.client.chat.update({
+//       token: context.botToken,
+//       // ts of message to update
+//       ts: body.message.ts,
+//       // Channel of message
+//       channel: body.channel.id,
+//       blocks: [
+//         {
+//           type: 'section',
+//           text: {
+//             type: 'mrkdwn',
+//             text: '*The button was clicked!*'
+//           }
+//         }
+//       ],
+//       text: 'Message from Test App'
+//     });
+//     console.log(result);
+//   }
+//   catch (error) {
+//     console.error(error);
+//   }
+// });
