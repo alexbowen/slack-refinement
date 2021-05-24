90dd759bb89a9bad4343e3568230298ebeb28d6d
alexbowen <alexbowen123@googlemail.com>
2021-05-24 17:24:03 +0100

    new bot token

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.view('submit', async ({ ack, payload, body, view, client }) => {
     await client.chat.postMessage({
       token: client.token,
       channel: "C021ZNE8Q5S",
-      // "thread_ts": 
+      "thread_ts":  view.private_metadata,
       text: `Estimation submitted by ${body.user.name} for ${payload.blocks[2].label.text}`
     });
   }
